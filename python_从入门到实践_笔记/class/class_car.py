da50765fe291510110af44d98a939d09322652fd
cloud-mist <49445212+cloud-mist@users.noreply.github.com>
2020-05-02 17:24:33 +0800

    Add files via upload

---
+++ python_从入门到实践_笔记/class/class_car.py
@@ -0,0 +1,13 @@
+class Car():
+	"""docstring for Car"""
+	def __init__(self, make, model, year):
+		self.make = make
+		self.model = model
+		self.year = year
+
+	def get_name(self):
+		long_name = str(self.year) + ' ' + self.make + " " + self.model
+		return long_name.title()	
+
+my_new_car = Car('audi', 'a4', 2016)
+print(my_new_car.get_name())
