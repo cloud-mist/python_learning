6c2cc2da7ae70d9809241465930fffa789e96145
cloud_mist <1131350950@qq.com>
2020-05-04 14:56:48 +0800

    学习笔记项目

---
+++ python_从入门到实践_笔记/learning_log/ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!f:\code_record\python_learning\python_从入门到实践_笔记\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
