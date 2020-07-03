6ebc0711e0b14705a71bd00db3d0b8ea337a360d
cloud <1131350950@qq.com>
2020-07-03 18:40:46 +0800

    update

--- Python_Crash_Course/learning_log/ll_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!f:\code_record\python_learning\python_从入门到实践_笔记\learning_log\ll_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
